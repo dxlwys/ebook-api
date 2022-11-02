3fb9494a0a91d8ada69d4bdc8b1c2a303d5b18e6
choihynwo <bimafaiz92@gmail.com>
2022-11-02 07:44:22 +0700

    second commit

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,10 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\HeloController;
+use App\Http\Controllers\SiswaController;
+use App\Http\Controllers\BookController;
+
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -14,6 +18,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('halo', function(){
+    return ['me' => 'Guanteng'];
 });
+
+Route::resource('halcontroller', HeloController::class);
+
+Route::resource('siswa', SiswaController::class);
+Route::resource('books', BookController::class);
